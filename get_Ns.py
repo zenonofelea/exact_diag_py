740be0c751b0411cdfdec93473df7ec2a1fa9642
Phillip Weinberg <weinbe58@bu.edu>
2016-06-25 10:11:06 -0400

    update repo

--- get_Ns.py
+++ get_Ns.py
@@ -11,8 +11,10 @@ for L in xrange(1,33,1):
 			for kblock in xrange(L/a):
 				blocks.append((L,{'kblock':kblock,'a':a}))
 
+	if L%2 ==0: Nmax=L/2+1
+	else: Nmax = L/2+1
 
-	for Nup in xrange(L+1):
+	for Nup in xrange(Nmax):
 		for a in xrange(1,L):
 			if L%a == 0:
 				for kblock in xrange(L/a):
@@ -35,10 +37,13 @@ with open("exact_diag_py/basis/basis1d/basis1d_Ns.py","w") as IO:
 			Nup=b._blocks.get('Nup')
 			a=b._blocks.get('a')
 			tup = (L,a,Nup,kblock)
+#			print b.Ns,tup
 			if b.Ns != 0:
 				print b.Ns,tup
 				IO.write(line.format(tup,b.Ns))
-		except ValueError:	continue
+		except ValueError,e:	
+			print e
+			continue
 
 	IO.write("}\n")
 	
